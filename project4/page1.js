43a991d45b2ab5a0c177376ed93adf0cab9b3fed
hmkupp <heikemkupp@MacBook-Pro-de-Heike.local>
2015-12-09 18:28:50 -0800

    sure

--- project4/page1.js
+++ project4/page1.js
@@ -8,9 +8,9 @@
 function changeImage() {
     var image = document.getElementById('gift');
     if (image.src.match("box")) {
-        image.src = "blue-open.gif";
+        image.src = "git-blue.png";
     } else {
-        image.src = "gift-blue.png";
+        image.src = "blue-open.gif";
     }
 }
 
